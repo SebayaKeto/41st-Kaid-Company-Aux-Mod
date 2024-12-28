2655db8af7214a46a615d56b0adb42a7d4eee1dd
Vitorzim <71033364+Vitor-Zim@users.noreply.github.com>
2024-12-28 20:55:18 -0300

    Invisible and Jorge Jumppacks
    Needs to hide the jumppacks from the players (I Don't remember the exact value).

--- PUBLISHED MODPACK/41st_Armor/config.cpp
+++ PUBLISHED MODPACK/41st_Armor/config.cpp
@@ -143,6 +143,7 @@ class CfgPatches
 			"FST_P2_NotSkilled",
 			"FST_P2_Charge",
 			"FST_P2_Hex",
+			"FST_P2_Viz",
 			"FST_P2_Tex",
 			"FST_P2_Hazard",
 			"FST_P2_Stein",
@@ -11072,6 +11073,140 @@ class CfgVehicles
 			{
 				"Short Jump",
 				{12,2,100,0,1,0}
+			}
+		};
+		class TransportItems
+		{
+		};
+	};
+	class FST_Backpack_Jumppack_Invisible: JLTS_Clone_jumppack_mc
+	{
+		author="Viz";
+		scope=2;
+		scopecurator=2;
+		scopearsenal=2;
+		maximumload=250;
+		displayName="[41st] Jump Pack (Daara Exclusive)";
+		model="\MRC\JLTS\characters\CloneArmor\CloneJumppackMC.p3d";
+		picture="\MRC\JLTS\characters\CloneArmor\data\ui\Clone_jumppack_mc_ui_ca.paa";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			""
+		};
+/* 		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+		tf_encryptionCode="tf_west_radio_code";
+		tf_dialog="anarc210_radio_dialog";
+		tf_subtype="digital_lr";
+		tf_range=50000; */
+		tf_hasLRradio=0;
+		JLTS_isJumppack=0;
+		DBA_jumppack_is_jumppack=1;
+		DBA_jumppack_spam_delay=0;
+		DBA_jumppack_energy_capacity=150;
+		DBA_jumppack_recharge=10;
+		DBA_jumppack_jump_effect_script="DBA_jumppack_effect_fnc_jt_21";
+		DBA_jumppack_effect_points[]=
+		{
+			
+			{
+				"spine3",
+				{0,-0.30000001,-0.1}
+			}
+		};
+		DBA_jumppack_sound_ignite[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21Start.ogg"
+		};
+		DBA_jumppack_sound_land[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21End.ogg"
+		};
+		DBA_jumppack_sound_idle[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21Idle.ogg"
+		};
+		DBA_jumppack_jump_types[]=
+		{
+			
+			{
+				"Forward Jump",
+				{12,20,50,0,0,0}
+			},
+			
+			{
+				"Short Jump",
+				{12,5,30,0,1,0}
+			}
+		};
+		class TransportItems
+		{
+		};
+	};
+	class FST_Backpack_JORGE_Jumppack: JLTS_Clone_jumppack_mc
+	{
+		author="Viz";
+		scope=2;
+		scopecurator=2;
+		scopearsenal=2;
+		maximumload=250;
+		displayName="[41st] Jump Pack (Jorge)";
+		model="\MRC\JLTS\characters\CloneArmor\CloneJumppackMC.p3d";
+		picture="\MRC\JLTS\characters\CloneArmor\data\ui\Clone_jumppack_mc_ui_ca.paa";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\41st_Armor\Data\Equipment\FST_AB_Jumppack_Jorge.paa"
+		};
+/* 		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+		tf_encryptionCode="tf_west_radio_code";
+		tf_dialog="anarc210_radio_dialog";
+		tf_subtype="digital_lr";
+		tf_range=50000; */
+		tf_hasLRradio=0;
+		JLTS_isJumppack=0;
+		DBA_jumppack_is_jumppack=1;
+		DBA_jumppack_spam_delay=0;
+		DBA_jumppack_energy_capacity=150;
+		DBA_jumppack_recharge=10;
+		DBA_jumppack_jump_effect_script="DBA_jumppack_effect_fnc_jt_21";
+		DBA_jumppack_effect_points[]=
+		{
+			
+			{
+				"spine3",
+				{0,-0.30000001,-0.1}
+			}
+		};
+		DBA_jumppack_sound_ignite[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21Start.ogg"
+		};
+		DBA_jumppack_sound_land[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21End.ogg"
+		};
+		DBA_jumppack_sound_idle[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21Idle.ogg"
+		};
+		DBA_jumppack_jump_types[]=
+		{
+			
+			{
+				"Forward Jump",
+				{12,20,50,0,0,0}
+			},
+			
+			{
+				"Short Jump",
+				{12,5,30,0,1,0}
 			}
 		};
 		class TransportItems
