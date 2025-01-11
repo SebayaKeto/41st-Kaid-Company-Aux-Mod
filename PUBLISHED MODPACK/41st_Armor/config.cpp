924fafe0e5359965b06d998231939d796bf09db5
Goldman35 <thibaultwilliam@yahoo.ca>
2025-01-11 12:26:11 -0500

    Added custom armor
    -Added Noble's AB helmet
    -Updated Overwatch's uniform
    -Updated Rumble's uniform

--- PUBLISHED MODPACK/41st_Armor/config.cpp
+++ PUBLISHED MODPACK/41st_Armor/config.cpp
@@ -5239,6 +5239,58 @@ class CfgWeapons
 			};
 		};
 	}; 
+	class FST_Airborne_Helmet_Noble: JLTS_CloneHelmetAB
+	{
+		author="Gold";
+		displayName="[41st] Airborne Helmet (Noble)";
+		scope=2;
+		ace_hearing_protection=1;
+        ace_hearing_lowerVolume=0;
+		picture="\MRC\JLTS\characters\CloneArmor\data\ui\CloneHelmetAB_ui_ca.paa";
+		model="\MRC\JLTS\characters\CloneArmor\CloneHelmetAB.p3d";
+		hiddenselections[]=
+		{
+			"camo1",
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Armor\data\Helmets\FST_AB_Helmet_Noble.paa",
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"41st_Armor\Data\Other\Clone_Helmet_AB.rvmat";
+		};
+		subItems[]=
+		{
+			"k_nvg"
+		};
+		class Iteminfo: HeadgearItem
+		{
+			uniformModel="\MRC\JLTS\characters\CloneArmor\CloneHelmetAB.p3d";
+			mass=15;
+			hiddenselections[]=
+			{
+				"camo1",
+			};
+			allowedSlots[]={801,901,701,605};
+			modelSides[]={6};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitpointname="HitHead";
+					armor=6;
+					PassThrough=0.08975;
+				};
+				class Face
+				{
+					hitpointName="HitFace";
+					armor=6;
+					PassThrough=0.08975;
+				};
+			};
+		};
+	}; 
 	class FST_Airborne_Helmet_Silhouette: JLTS_CloneHelmetAB
 	{
 		author="Gold";
