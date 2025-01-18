e643a3dd3acf435b623935373d93808e7c2d081e
Vitorzim <71033364+Vitor-Zim@users.noreply.github.com>
2025-01-18 13:37:24 -0300

    fixes are back

--- PUBLISHED MODPACK/41st_Weapons/Vehicle/config.cpp
+++ PUBLISHED MODPACK/41st_Weapons/Vehicle/config.cpp
@@ -17,6 +17,7 @@ class Mode_SemiAuto;
 class Mode_FullAuto;
 class CfgAmmo
 {
+	class Cannon_30mm_HE_Plane_CAS_02_F;
 	class BulletBase;
 	class FST_127x99_red: BulletBase
 	{
@@ -403,6 +404,23 @@ class CfgAmmo
 		indirectHitRange=8;
 		caliber=2;
 	};
+	class FST_LAAT_Ball_Laser_Ammo: Cannon_30mm_HE_Plane_CAS_02_F
+	{
+		author="Ruby, Code Inherted From Viz.";
+		displayname="LAAT Ball Turret";
+		warheadName="LAAT Ball Turret";
+		hit=75;
+		indirectHit=30;
+		deflecting=0;
+		indirectHitRange=6;
+		caliber=20;
+		explosive=1;
+		allowAgainstInfantry=1;
+		airlock=1;
+		model="3as\3as_weapons\data\tracer_green.p3d";
+		effectFly="3AS_PlasmaBolt_Medium_Green_Fly";
+		aiAmmoUsageFlags="128 + 512";
+	};
 };
 class CfgMagazines
 {
@@ -437,6 +455,16 @@ class CfgMagazines
 		tracersEvery=1;
 		initSpeed=800;
 		mass=20;
+	};
+	class FST_LAAT_350_Laser_Round: VehicleMagazine
+	{
+		displayName="30mm LAAT Ball Cannon";
+		displayNameShort="LAAT BRR Mode";
+		count=350;
+		model="a3\weapons_f\empty.p3d";
+		muzzleImpulseFactor[]={0.0000001,0.022};
+		nameSound="mgun";
+		ammo="FST_LAAT_Ball_Laser_Ammo";
 	};
 	class FST_thermal_Y_Wing: 1Rnd_HE_Grenade_shell
 	{
@@ -565,6 +593,11 @@ class CfgWeapons
 	class autocannon_Base_F;
 	class player;
 	class OPTRE_M247T_Coax;
+	class CannonCore;
+	class Cannon_30mm_Plane_CAS_02_F: CannonCore
+	{
+		class LowROF;
+	};
 	class ls_laat_gun;
 	
 	//AAT------------------------------------------------
@@ -979,14 +1012,19 @@ class CfgWeapons
 			};
 		};
 	};
-	class Cannon_30mm_Plane_CAS_02_F;
 	class FST_30mm_Autocannon_LAAT: Cannon_30mm_Plane_CAS_02_F
 	{
 		displayName="Medium LAAT Blaster Cannon";
 		author="Daara";
 		cursor="EmptyCursor";
-		reloadTime=0.03;
-		fireLightDiffuse[]={0,1,0};
+		magazines[]=
+		{
+			"FST_thermal_coil",
+		};
+		reloadTime=0.175;
+		magazineReloadTime=10;
+		burst=1;
+		fireLightDiffuse[]={1,0,0};
 		class GunParticles
 		{
 			class Effect
@@ -1002,66 +1040,119 @@ class CfgWeapons
 				effectName="";
 			};
 		};
-		muzzles[]=
+		class LowROF: LowROF
 		{
-			"HE",
-		};
-		class HE: autocannon_Base_F
-		{
-			displayName="30mm LAAT Blaster Cannon";
-			cursor="EmptyCursor";
-			magazines[]=
-			{
-				"FST_thermal_coil"
-			};
 			magazineReloadTime=10;
-			reloadTime=0.03;
-			class player: player
+			reloadTime=0.175;
+			burst=1;
+			displayname="";
+			class BaseSoundModeType;
+			class StandardSound: BaseSoundModeType
 			{
-				class StandardSound
+				begin1[]=
 				{
-					begin1[]=
-					{
-						"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
-						2.5118864,
-						1,
-						1500
-					};
-					begin2[]=
-					{
-						"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
-						2.5118864,
-						1.015,
-						1500
-					};
-					begin3[]=
-					{
-						"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
-						2.5118864,
-						0.985,
-						1500
-					};
-					begin4[]=
-					{
-						"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
-						2.5118864,
-						1.01,
-						1500
-					};
-					begin5[]=
-					{
-						"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
-						2.5118864,
-						0.995,
-						1500
-					};
-					soundBegin[]=
-					{
-						"begin1",
-						1
-					};
-					dispersion=0.0018;
-					reloadTime=0.03;
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					1,
+					1500
+				};
+				begin2[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					1.015,
+					1500
+				};
+				begin3[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					0.985,
+					1500
+				};
+				begin4[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					1.01,
+					1500
+				};
+				begin5[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					0.995,
+					1500
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					1
+				};
+			};
+		};
+	};
+	class FST_LAAT_Ball_Turret: Cannon_30mm_Plane_CAS_02_F
+	{
+		displayName="[41st] Ball Turret Cannon";
+		canLock=0;
+		ballisticsComputer="4 + 2 + 8";
+		magazines[]=
+		{
+			"FST_LAAT_350_Laser_Round"
+		};
+		reloadTime=0.074999999;
+		dispersion=0.0094999997;
+		magazineReloadTime=10;
+		burst=1;
+		class LowROF: LowROF
+		{
+			reloadtime=0.074999998;
+			magazineReloadTime=10;
+			burst=1;
+			displayname="";
+			class BaseSoundModeType;
+			class StandardSound: BaseSoundModeType
+			{
+				begin1[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					1,
+					1500
+				};
+				begin2[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					1.015,
+					1500
+				};
+				begin3[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					0.985,
+					1500
+				};
+				begin4[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					1.01,
+					1500
+				};
+				begin5[]=
+				{
+					"\41st_weapons\Vehicle\Sounds\FST_30mm_cannon.ogg",
+					2.5118864,
+					0.995,
+					1500
+				};
+				soundBegin[]=
+				{
+					"begin1",
+					1
 				};
 			};
 		};
@@ -1151,7 +1242,6 @@ class CfgWeapons
 			};
 		};
 	};
-	class CannonCore;
 	class FST_Mass_Driver_Cannon: CannonCore
 	{
 		scope=1;
