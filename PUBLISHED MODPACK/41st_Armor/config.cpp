f491bc64b3a6a117da0bfdf12a6dd1930a39cd7e
Vitorzim <71033364+Vitor-Zim@users.noreply.github.com>
2025-01-20 17:45:04 -0300

    Helmet,Armor,Face Jumppack
    Helmets: Sledge (add), Man (Update);
    Armor: Vector (update), Deso (Update);
    Face: Cardinal (add);
    Jumppack: Sunny (add)

--- PUBLISHED MODPACK/41st_Armor/config.cpp
+++ PUBLISHED MODPACK/41st_Armor/config.cpp
@@ -192,6 +192,7 @@ class CfgPatches
 			"FST_P2_Woodland",
 			"FST_P2_Silhouette",
 			"FST_P2_Lucky",
+			"FST_P2_Sledge",
 			//"FST_P2_Jaws",
 			"FST_P2_Raven",
 			//////////OTHERS//////////
@@ -8465,6 +8466,16 @@ class CfgWeapons
 			"41st_Armor\Data\Helmets\FST_P2_Helmet_Cooker.paa",
 		};
 	};
+	class FST_P2_Helmet_Sledge: FST_P2_Helmet_Base
+	{
+		author="Viz";
+		scope=2;
+		displayName="[41st] P2 Helmet [Sledge]";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Armor\Data\Helmets\FST_P2_Helmet_Sledge.paa",
+		};
+	};
 	//////////////UNIFORMS//////////////
 	class FST_Uniform_Bodyglove: lsd_gar_bodyGlove_uniform
 	{
@@ -12749,7 +12760,7 @@ class CfgVehicles
 		scope=2;
 		scopecurator=2;
 		scopearsenal=2;
-		maximumload=250;
+		maximumload=400;
 		displayName="[41st] Jump Pack (Invisible)";
 		model="\MRC\JLTS\characters\CloneArmor\CloneJumppackMC.p3d";
 		picture="\MRC\JLTS\characters\CloneArmor\data\ui\Clone_jumppack_mc_ui_ca.paa";
@@ -12761,12 +12772,12 @@ class CfgVehicles
 		{
 			""
 		};
-/* 		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+ 		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
 		tf_encryptionCode="tf_west_radio_code";
 		tf_dialog="anarc210_radio_dialog";
 		tf_subtype="digital_lr";
-		tf_range=50000; */
-		tf_hasLRradio=0;
+		tf_range=50000;
+		tf_hasLRradio=1;
 		JLTS_isJumppack=0;
 		DBA_jumppack_is_jumppack=1;
 		DBA_jumppack_spam_delay=1;
@@ -13490,6 +13501,68 @@ class CfgVehicles
 		hiddenSelectionsTextures[]=
 		{
 			"\41st_Armor\Data\Equipment\FST_AB_Jumppack_James.paa"
+		};
+		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+		tf_encryptionCode="tf_west_radio_code";
+		tf_dialog="anarc210_radio_dialog";
+		tf_subtype="digital_lr";
+		tf_range=50000;
+		tf_hasLRradio=1;
+		JLTS_isJumppack=0;
+		DBA_jumppack_is_jumppack=1;
+		DBA_jumppack_spam_delay=1;
+		DBA_jumppack_energy_capacity=100;
+		DBA_jumppack_recharge=7.6;
+		DBA_jumppack_jump_effect_script="DBA_jumppack_effect_fnc_jt_21";
+		DBA_jumppack_effect_points[]=
+		{
+			
+			{
+				"spine3",
+				{0,-0.30000001,-0.1}
+			}
+		};
+		DBA_jumppack_sound_ignite[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21Start.ogg"
+		};
+		DBA_jumppack_sound_land[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21End.ogg"
+		};
+		DBA_jumppack_sound_idle[]=
+		{
+			"DBA_Core\Addons\DBA_JumpPack\jumppack\Sounds\cdv21Idle.ogg"
+		};
+		DBA_jumppack_jump_types[]=
+		{
+			
+			{
+				"Short Jump",
+				{12,2,100,0,1,0}
+			}
+		};
+		class TransportItems
+		{
+		};
+	};
+	class FST_Backpack_Jumppack_Sunny: FST_Backpack_Jumppack
+	{
+		author="Viz";
+		scope=2;
+		scopecurator=2;
+		scopearsenal=2;
+		maximumload=250;
+		displayName="[41st] Jump Pack (Sunny)";
+		model="\MRC\JLTS\characters\CloneArmor\CloneJumppackMC.p3d";
+		picture="\MRC\JLTS\characters\CloneArmor\data\ui\Clone_jumppack_mc_ui_ca.paa";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\41st_Armor\Data\Equipment\FST_AB_Jumppack_Sunny.paa"
 		};
 		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
 		tf_encryptionCode="tf_west_radio_code";
@@ -20837,7 +20910,7 @@ class CfgVehicles
 		hiddenSelectionsTextures[]=
 		{
 			"41st_Armor\Data\Uniforms\FST_P2_BodyUpper_Deso.Paa",
-			"41st_Armor\Data\Uniforms\FST_P2_BodyLower_Deso.Paa",
+			"41st_Armor\Data\Uniforms\FST_P2_BodyLower_Deso.Paa"
 		};
 		weapons[]=
 		{
@@ -23884,6 +23957,7 @@ class CfgFaces
 		};
 		class FST_Clone_Head_Burns: FST_clone_Default_head
 		{
+			author="Viz"
 			name="Clone Trooper Burns";
 			displayName="[41st] Clone Trooper - Burns";
 			material="\lsd_identities\data\models\data\clone_b.rvmat";
@@ -23896,6 +23970,14 @@ class CfgFaces
 			displayName="[41st] Clone Trooper - Sigs";
 			material="\lsd_identities\data\models\data\clone_b.rvmat";
 			texture="41st_Armor\Data\Faces\FST_Clone_Sigs.paa";
+		};
+		class FST_Clone_Head_Cardinal: FST_clone_Default_head
+		{
+			author="Viz"
+			name="Clone Trooper Cardinal";
+			displayName="[41st] Clone Trooper - Cardinal";
+			material="\lsd_identities\data\models\data\clone_b.rvmat";
+			texture="41st_Armor\Data\Faces\FST_Clone_Cardinal.paa";
 		};
 	};
 };
