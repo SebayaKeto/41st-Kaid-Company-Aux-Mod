d1cc4fffb285bd59dfa6a8d54b551d79daf90f49
Goldman35 <thibaultwilliam@yahoo.ca>
2025-03-08 10:45:43 -0500

    Corrected incorrect warthog faction

--- PUBLISHED MODPACK/41st_Vehicles/Warthog/config.cpp
+++ PUBLISHED MODPACK/41st_Vehicles/Warthog/config.cpp
@@ -14,7 +14,7 @@ class CfgPatches
 			//"FST_Technical_Twin_Laser",
 			"FST_Technical_Twin_Laser_White",
 			//"FST_Technical_Twin_Laser_Snow",
-			"FST_Technical_Transport",
+			//"FST_Technical_Transport",
 			"FST_Technical_Transport_White",
 			"FST_Technical_Transport_Indep"
 		};
@@ -1350,14 +1350,294 @@ class CfgVehicles
 		class TransportWeapons{};
 		class TransportBackpacks{};
 	};
-	class FST_Technical_Transport_Indep: FST_Technical_Transport
-	{	
-		author="Gold";
+	class FST_Technical_Transport_Indep: OPTRE_M813_TT
+	{
+		author="Daara";
 		displayName="[41st] Militia Technical Transport (Indep)";
+		model="\OPTRE_Vehicles\Warthog\M12_MAAV_TT.p3d";
 		faction="FST_Independent_Faction";
 		editorSubcategory="FST_Ground_Vehicle";
-		side=3;
-	};	
+		scope=2;
+		side=2;
+		scopeCurator=2;
+		tas_canBlift=1;
+        tas_liftVars="[[[[0,-3.7,-7.8]]], [0], [0]]"; 
+		transportSoldier=0;
+		crew="";
+		maxSpeed=202;
+		fuelCapacity=50;
+		normalSpeedForwardCoef=0.5;
+		slowSpeedForwardCoef=0.25;
+		class complexGearbox
+		{
+			GearboxRatios[]=
+			{
+				"R1",
+				-6,
+				"N",
+				0,
+				"D1",
+				4.65,
+				"D2",
+				3.20,
+				"D3",
+				2.40,
+				"D4",
+				1.87,
+				"D5",
+				1.54,
+				"D6",
+				1.29,
+				"D7",
+				1.11,
+				"D8",
+				0.97,
+				"D9",
+				0.87,
+				"D10",
+				0.78,
+			};
+			AmphibiousRatios[]=
+			{
+				"R1",
+				-6,
+				"N",
+				0,
+				"D1",
+				4.65,
+				"D2",
+				3.20,
+				"D3",
+				2.40,
+				"D4",
+				1.87,
+				"D5",
+				1.54,
+				"D6",
+				1.29,
+				"D7",
+				1.11,
+				"D8",
+				0.97,
+				"D9",
+				0.87,
+				"D10",
+				0.78,
+			};
+			TransmissionRatios[]=
+			{
+				"High",
+				5.45
+			};
+			gearBoxMode="auto";
+			moveOffGear=1;
+			driveString="D";
+			neutralString="N";
+			reverseString="R";
+		};
+		class Turrets: Turrets
+		{
+			class CargoGunner_1: NewTurret
+			{
+				gunnerInAction="passenger_inside_2";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				gunnerCompartments="Compartment1";
+				memoryPointsGetInGunner="pos pas";
+				memoryPointsGetInGunnerDir="pos pas dir";
+				outGunnerMayFire=1;
+				inGunnerMayFire=1;
+				memoryPointGunnerOptics="";
+				gunnerName="Front Passenger";
+				proxyIndex=1;
+				proxyType="CPCargo";
+				showAsCargo=1;
+				maxElev=30;
+				minElev=-20;
+				maxTurn=95;
+				minTurn=-95;
+				animationSourceHatch="cargo_front";
+				enabledByAnimationSource="";
+				LODTurnedIn=1200;
+				LODTurnedOut=-1;
+				viewGunnerInExternal=1;
+				canHideGunner=1;
+				hasGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=0;
+				isPersonTurret=2;
+				weapons[]={};
+				magazines[]={};
+				primaryGunner=0;
+				primaryObserver=0;
+				gunnerOpticsShowCursor=0;
+				body="";
+				gun="";
+				animationSourceBody="";
+				animationSourceGun="";
+				soundServo[]=
+				{
+					"",
+					"db-50",
+					1
+				};
+				startEngine=0;
+				hideWeaponsGunner=0;
+				gunnerGetInAction="GetInHigh";
+				gunnerGetOutAction="GetOutHigh";
+				commanding=-1;
+				gunnerOpticsModel="";
+				gunnerOutOpticsColor[]={0,0,0,1};
+				gunnerForceOptics=0;
+				gunnerOutForceOptics=0;
+				gunnerOutOpticsShowCursor=0;
+				gunnerOpticsEffect[]={};
+				stabilizedInAxes=0;
+			};
+			class CargoGunner_02: CargoGunner_1
+			{
+				gunnerAction="passenger_bench_1";
+				gunnerInAction="passenger_bench_1";
+				gunnerName="Right Passenger (Rear)";
+				memoryPointsGetInGunner="pos pasrr";
+				memoryPointsGetInGunnerDir="pos pasrr dir";
+				proxyIndex=4;
+				gunnerCompartments="Compartment2";
+				canHideGunner=0;
+				inGunnerMayFire=1;
+			};
+			class CargoGunner_03: CargoGunner_02
+			{
+				gunnerAction="passenger_bench_1";
+				gunnerInAction="passenger_bench_1";
+				gunnerName="Left Passenger (Rear)";
+				memoryPointsGetInGunner="pos paslr";
+				memoryPointsGetInGunnerDir="pos paslr dir";
+				proxyIndex=5;
+				gunnerCompartments="Compartment2";
+				canHideGunner=0;
+			};
+			class CargoGunner_04: CargoGunner_02
+			{
+				gunnerAction="passenger_inside_2";
+				gunnerInAction="passenger_inside_2";
+				gunnerName="Right Passenger (Middle)";
+				memoryPointsGetInGunner="pos pasrm";
+				memoryPointsGetInGunnerDir="pos pasrm dir";
+				proxyIndex=6;
+				gunnerCompartments="Compartment2";
+				canHideGunner=0;
+			};
+			class CargoGunner_05: CargoGunner_02
+			{
+				gunnerAction="passenger_inside_2";
+				gunnerInAction="passenger_inside_2";
+				gunnerName="Left Passenger (Middle)";
+				memoryPointsGetInGunner="pos paslm";
+				memoryPointsGetInGunnerDir="pos paslm dir";
+				proxyIndex=7;
+				gunnerCompartments="Compartment2";
+				canHideGunner=0;
+			};
+			class CargoGunner_06: CargoGunner_02
+			{
+				gunnerAction="passenger_inside_2";
+				gunnerInAction="passenger_inside_2";
+				gunnerName="Right Passenger (Front)";
+				memoryPointsGetInGunner="pos pasrf";
+				memoryPointsGetInGunnerDir="pos pasrf dir";
+				proxyIndex=8;
+				gunnerCompartments="Compartment2";
+				canHideGunner=0;
+			};
+			class CargoGunner_07: CargoGunner_02
+			{
+				gunnerAction="passenger_inside_2";
+				gunnerInAction="passenger_inside_2";
+				gunnerName="Left Passenger (Front)";
+				memoryPointsGetInGunner="pos paslf";
+				memoryPointsGetInGunnerDir="pos paslf dir";
+				proxyIndex=9;
+				gunnerCompartments="Compartment2";
+				canHideGunner=0;
+			};
+		};
+		tf_range=25000;
+		tf_isolatedAmount=0.40000001;
+		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio=1;
+		enableRadio=1;
+		class Sounds
+		{
+			soundSetsInt[]=
+			{
+				"Mrap_01_Engine_RPM0_INT_SoundSet",
+				"Mrap_01_Engine_RPM1_INT_SoundSet",
+				"Mrap_01_Engine_RPM2_INT_SoundSet",
+				"Mrap_01_Engine_RPM3_INT_SoundSet",
+				"Mrap_01_Engine_RPM4_INT_SoundSet",
+				"Mrap_01_Engine_INT_Burst_SoundSet",
+				"Mrap_01_Rattling_INT_SoundSet",
+				"Mrap_01_Stress_INT_SoundSet",
+				"Mrap_01_Rain_INT_SoundSet",
+				"Mrap_01_Tires_Rock_Fast_INT_SoundSet",
+				"Mrap_01_Tires_Grass_Fast_INT_SoundSet",
+				"Mrap_01_Tires_Sand_Fast_INT_SoundSet",
+				"Mrap_01_Tires_Gravel_Fast_INT_SoundSet",
+				"Mrap_01_Tires_Mud_Fast_INT_SoundSet",
+				"Mrap_01_Tires_Asphalt_Fast_INT_SoundSet",
+				"Mrap_01_Tires_Water_Fast_INT_SoundSet",
+				"Mrap_01_Tires_Rock_Slow_INT_SoundSet",
+				"Mrap_01_Tires_Grass_Slow_INT_SoundSet",
+				"Mrap_01_Tires_Sand_Slow_INT_SoundSet",
+				"Mrap_01_Tires_Gravel_Slow_INT_SoundSet",
+				"Mrap_01_Tires_Mud_Slow_INT_SoundSet",
+				"Mrap_01_Tires_Asphalt_Slow_INT_SoundSet",
+				"Mrap_01_Tires_Water_Slow_INT_SoundSet",
+				"Mrap_01_Tires_Turn_Hard_INT_SoundSet",
+				"Mrap_01_Tires_Turn_Soft_INT_SoundSet",
+				"Mrap_01_Tires_Brake_Hard_INT_SoundSet",
+				"Mrap_01_Tires_Brake_Soft_INT_SoundSet",
+				"",
+				"Tires_Movement_Dirt_Int_01_SoundSet"
+			};
+			soundSetsExt[]=
+			{
+				"Mrap_01_Engine_RPM0_EXT_SoundSet",
+				"Mrap_01_Engine_RPM1_EXT_SoundSet",
+				"Mrap_01_Engine_RPM2_EXT_SoundSet",
+				"Mrap_01_Engine_RPM3_EXT_SoundSet",
+				"Mrap_01_Engine_RPM4_EXT_SoundSet",
+				"Mrap_01_Engine_EXT_Burst_SoundSet",
+				"Mrap_01_Rattling_EXT_SoundSet",
+				"Mrap_01_Stress_EXT_SoundSet",
+				"Mrap_01_Rain_EXT_SoundSet",
+				"Mrap_01_Tires_Rock_Fast_EXT_SoundSet",
+				"Mrap_01_Tires_Grass_Fast_EXT_SoundSet",
+				"Mrap_01_Tires_Sand_Fast_EXT_SoundSet",
+				"Mrap_01_Tires_Gravel_Fast_EXT_SoundSet",
+				"Mrap_01_Tires_Mud_Fast_EXT_SoundSet",
+				"Mrap_01_Tires_Asphalt_Fast_EXT_SoundSet",
+				"Mrap_01_Tires_Water_Fast_EXT_SoundSet",
+				"Mrap_01_Tires_Rock_Slow_EXT_SoundSet",
+				"Mrap_01_Tires_Grass_Slow_EXT_SoundSet",
+				"Mrap_01_Tires_Sand_Slow_EXT_SoundSet",
+				"Mrap_01_Tires_Gravel_Slow_EXT_SoundSet",
+				"Mrap_01_Tires_Mud_Slow_EXT_SoundSet",
+				"Mrap_01_Tires_Asphalt_Slow_EXT_SoundSet",
+				"Mrap_01_Tires_Water_Slow_EXT_SoundSet",
+				"Mrap_01_Tires_Turn_Hard_EXT_SoundSet",
+				"Mrap_01_Tires_Turn_Soft_EXT_SoundSet",
+				"Mrap_01_Tires_Brake_Hard_EXT_SoundSet",
+				"Mrap_01_Tires_Brake_Soft_EXT_SoundSet",
+				"",
+				"Tires_Movement_Dirt_Ext_01_SoundSet"
+			};
+		};
+		class TransportMagazines{};
+		class TransportItems{};
+		class TransportWeapons{};
+		class TransportBackpacks{};
+	};
 };
 class Mode_FullAuto;
 class CfgWeapons
