559a2bcce722f8d083c740d888479742125a1a0f
MaldovaMods <62029006+WilliamHolbert@users.noreply.github.com>
2025-04-20 09:52:19 -0700

    Maldovas Mass Update
    Not everything is completely done but the textures are all there.  I will have time tonight to get everything working

--- FST_Barricades/config.cpp
+++ FST_Barricades/config.cpp
@@ -19,7 +19,8 @@ class CfgPatches
 			"FST_Barricade_SecurityBarrier_Arid",
 			"FST_Barricade_SecurityBarrier_Urban",
 			"FST_Barricade_SecurityBarrier_Jungle",
-			"FST_Barricade_SecurityBarrier_Command"
+			"FST_Barricade_SecurityBarrier_Command",
+			"FST_Barricade_SecurityBarrierShieldWide"
 		};
 		weapons[]={};
 	};
@@ -54,7 +55,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	// Overgrown Base Variation
 	class FST_Barricade_SecurityBarrier_Overgrown: FST_Barricade_SecurityBarrier_Base
@@ -64,7 +65,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Overgrown)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_Overgrown_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_Overgrown_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	//Inverted Colors
 	class FST_Barricade_SecurityBarrier_Inverted: FST_Barricade_SecurityBarrier_Base
@@ -74,7 +75,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Inverted)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_41stInverted_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_41stInverted_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	//Camo Colors
 	class FST_Barricade_SecurityBarrier_Sand: FST_Barricade_SecurityBarrier_Base
@@ -84,7 +85,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Sand)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_Sand_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_Sand_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	class FST_Barricade_SecurityBarrier_Woodland: FST_Barricade_SecurityBarrier_Base
 	{
@@ -93,7 +94,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Woodland)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_Woodland_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_Woodland_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	class FST_Barricade_SecurityBarrier_Tundra: FST_Barricade_SecurityBarrier_Base
 	{
@@ -102,7 +103,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Tundra)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_Tundra_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_Tundra_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	class FST_Barricade_SecurityBarrier_Arid: FST_Barricade_SecurityBarrier_Base
 	{
@@ -111,7 +112,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Arid)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_Arid_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_Arid_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	class FST_Barricade_SecurityBarrier_Urban: FST_Barricade_SecurityBarrier_Base
 	{
@@ -120,7 +121,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Urban)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_Urban_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_Urban_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	class FST_Barricade_SecurityBarrier_Jungle: FST_Barricade_SecurityBarrier_Base
 	{
@@ -129,7 +130,7 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Jungle)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_JungleOvergrown_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_JungleOvergrown_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
 	};
 	// Specialized Textures
 	class FST_Barricade_SecurityBarrier_Command: FST_Barricade_SecurityBarrier_Base
@@ -139,6 +140,21 @@ class CfgVehicles
 		displayName = "[41st] Security Barrier (Command)";
 		hiddenSelections[] = {"Camo1","Plasteel", "Steel", "Camo1Low"};
 		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier_Command_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrier_Command_co.paa", "FST\FST_Barricades\Data\Textures\FSTBarrierLow_co.paa"};
-		hiddenSelctionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrier.rvmat", "FST\FST_Barricades\Data\Textures\FSTBarrierLow.rvmat"};
+	};
+	// Shielded Security Barricade
+	class FST_Barricade_SecurityBarrierShieldWide_Base: FST_Barricade_SecurityBarrier_Base
+	{
+		displayName = "[41st] Security Barrier (Shield)";
+		model = "FST\FST_Barricades\41st_SecurityBarrierShieldWide.p3d";
+	};
+	class FST_Barricade_SecurityBarrierShieldWide: FST_Barricade_SecurityBarrierShieldWide_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "[41st] Shielded Security Barrier";
+		hiddenSelections[] = {"Camo1","Plasteel", "Shield"};
+		hiddenSelectionsTextures[] = {"FST\FST_Barricades\Data\Textures\FSTBarrierShield_co.paa","FST\FST_Common\Data\Textures\Plasteel\Steel_co.paa", "FST\FST_Common\Data\Textures\Shield\BlueShield_co.paa"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Barricades\Data\Textures\FSTBarrierShield.rvmat", "FST\FST_Common\Data\Textures\Plasteel\Steel.rvmat", "FST\FST_Common\Data\Textures\Shield\Shield.rvmat"};
 	};
 };
