28619b434451a1991480f1d65673d6407424b8b4
MaldovaMods <62029006+WilliamHolbert@users.noreply.github.com>
2025-05-03 20:29:42 -0700

    Barricade Fixes
    Many fixes to barricade textures and other stuff.  Will post this in the discord when I have it fully working

--- FST_Vegetation/Vines/config.cpp
+++ FST_Vegetation/Vines/config.cpp
@@ -4,7 +4,7 @@ class CfgPatches
 {
 	class FST_Vegetation_Vines
 	{
-		units[] = {"FST_Vine_KashyyykOne"};
+		units[] = {"FST_Vine_KashyyykOne","FST_Vine_KashyyykWoodlandOne","FST_Vine_KashyyykSwampOne"};
 		weapons[] = {};
 		requiredAddons[] = {"A3_Data_F"};
 		version = 1.14;
@@ -14,17 +14,43 @@ class CfgPatches
 class CfgVehicles
 {
 	class Strategic;
-	class 3AS_Light_Coral: Strategic
+	class FST_Vine_KashyyykBase: Strategic
 	{
-		scope = 2;
+		scope = 1;
 		eden = 1;
-		model = "3as\Prop_Rugosa\Rugosa_light\Corral 1.p3d";
+		model = "FST\FST_Vegetation\Vines\KashyyykVinesOne.p3d";
 		placement = "vertical";
 		mapSize = 3;
-		displayName = "Kashyyyk Vines 1";
+		displayName = "Kashyyyk Vines Base";
 		faction = "prop";
-		texture = "3as\Prop_Rugosa\data\Coral123_co.paa";
+		hiddenSelections[] = {"Camo1"};
+		hiddenSelectionsTextures[] = {"FST\FST_Common\Data\Textures\JungleWood\JungleWoodOne_co.paa"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Common\Data\Textures\JungleWood\FST_KashyyykVineOne.rvmat"};
 		editorcategory = "FST_CatagoryVegetationKashyyyk";
 		editorsubcategory = "FST_Subcategory_KashyyykVines";
+	};
+	class FST_Vine_KashyyykJungleOne: FST_Vine_KashyyykBase
+	{
+		scope = 2;
+		displayName = "Kashyyyk Vines (Jungle 1)";
+		hiddenSelections[] = {"Camo1"};
+		hiddenSelectionsTextures[] = {"FST\FST_Common\Data\Textures\JungleWood\JungleWoodOne_co.paa"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Common\Data\Textures\JungleWood\FST_KashyyykVineOne.rvmat"};
+	};
+	class FST_Vine_KashyyykWoodlandOne: FST_Vine_KashyyykBase
+	{
+		scope = 2;
+		displayName = "Kashyyyk Vines (Woodland 1)";
+		hiddenSelections[] = {"Camo1"};
+		hiddenSelectionsTextures[] = {"FST\FST_Common\Data\Textures\JungleWood\JungleWoodTwo_co.paa"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Common\Data\Textures\JungleWood\FST_KashyyykVineTwo.rvmat"};
+	};
+	class FST_Vine_KashyyykSwampOne: FST_Vine_KashyyykBase
+	{
+		scope = 2;
+		displayName = "Kashyyyk Vines (Swamp 1)";
+		hiddenSelections[] = {"Camo1"};
+		hiddenSelectionsTextures[] = {"FST\FST_Common\Data\Textures\JungleWood\JungleWoodSwamp_co.paa"};
+		hiddenSelectionsMaterials[] = {"FST\FST_Common\Data\Textures\JungleWood\FST_KashyyykVineSwamp.rvmat"};
 	};
 };
