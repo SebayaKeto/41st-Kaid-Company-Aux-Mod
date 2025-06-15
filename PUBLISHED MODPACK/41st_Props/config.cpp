3dddbe27109c76d487bac69e017fe97daee8de34
Goldman35 <thibaultwilliam@yahoo.ca>
2025-06-15 01:01:24 -0400

    Added custom gweem baseplates (Normal and thin)

--- PUBLISHED MODPACK/41st_Props/config.cpp
+++ PUBLISHED MODPACK/41st_Props/config.cpp
@@ -34,6 +34,13 @@ class CfgFactionClasses
 		side=3;
 	};
 };
+class CfgEditorSubcategories
+{
+	class FST_Baseplates
+	{
+		displayName="41st Baseplates";
+	};
+};
 class CfgVehicles
 {
 	class Strategic;
@@ -42,9 +49,7 @@ class CfgVehicles
 	{
 		class DestructionEffects;
 	};
-	class 3AS_Prop_Base_Animate: House_F
-	{
-	};
+	class 3AS_Prop_Base_Animate: House_F{};
 	class Ruins_F;
 	class Lamps_base_F;
 	class ThingX;
@@ -159,6 +164,286 @@ class CfgVehicles
 		hiddenSelectionsTextures[]=
 		{
 			"41st_Props\Data\CIS_small_Wall_gate_co.paa"
+		};
+	};
+	class 3AS_Prop_Baseplate_10x10;
+	class FST_Prop_Baseplate_10: 3AS_Prop_Baseplate_10x10
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (10x10)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR.jpg";
+	};
+	class FST_Prop_Baseplate_10_Square: 3AS_Prop_Baseplate_10x10
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (10x10) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR_Square.jpg";
+	};
+	class 3AS_Prop_Baseplate_10x10_thin;
+	class FST_Prop_Baseplate_10_Thin: 3AS_Prop_Baseplate_10x10_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (10x10)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar\bp_50_gar.rvmat"
+		};
+	};
+	class FST_Prop_Baseplate_10_Thin_Square: 3AS_Prop_Baseplate_10x10_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (10x10) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar_s\bp_50_gar_s.rvmat"
+		};
+	};
+	class 3AS_Prop_Baseplate_20x20;
+	class FST_Prop_Baseplate_20: 3AS_Prop_Baseplate_20x20
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (20x20)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR.jpg";
+	};
+	class FST_Prop_Baseplate_20_Square: 3AS_Prop_Baseplate_20x20
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (20x20) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR_Square.jpg";
+	};
+	class 3AS_Prop_Baseplate_20x20_thin;
+	class FST_Prop_Baseplate_20_Thin: 3AS_Prop_Baseplate_20x20_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (20x20)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar\bp_50_gar.rvmat"
+		};
+	};
+	class FST_Prop_Baseplate_20_Thin_Square: 3AS_Prop_Baseplate_20x20_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (20x20) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar_s\bp_50_gar_s.rvmat"
+		};
+	};
+	class 3AS_Prop_Baseplate_30x30;
+	class FST_Prop_Baseplate_30: 3AS_Prop_Baseplate_30x30
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (30x30)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR.jpg";
+	};
+	class FST_Prop_Baseplate_30_Square: 3AS_Prop_Baseplate_30x30
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (30x30) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR_Square.jpg";
+	};
+	class 3AS_Prop_Baseplate_30x30_thin;
+	class FST_Prop_Baseplate_30_Thin: 3AS_Prop_Baseplate_30x30_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (30x30)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar\bp_50_gar.rvmat"
+		};
+	};
+	class FST_Prop_Baseplate_30_Thin_Square: 3AS_Prop_Baseplate_30x30_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (30x30) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar_s\bp_50_gar_s.rvmat"
+		};
+	};
+	class 3AS_Prop_Baseplate_40x40;
+	class FST_Prop_Baseplate_40: 3AS_Prop_Baseplate_40x40
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (40x40)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR.jpg";
+	};
+	class FST_Prop_Baseplate_40_Square: 3AS_Prop_Baseplate_40x40
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (40x40) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR_Square.jpg";
+	};
+	class 3AS_Prop_Baseplate_40x40_thin;
+	class FST_Prop_Baseplate_40_Thin: 3AS_Prop_Baseplate_40x40_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (40x40)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar\bp_50_gar.rvmat"
+		};
+	};
+	class FST_Prop_Baseplate_40_Thin_Square: 3AS_Prop_Baseplate_40x40_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (40x40) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar_s\bp_50_gar_s.rvmat"
+		};
+	};
+	class 3AS_Prop_Baseplate_50x50;
+	class FST_Prop_Baseplate_50: 3AS_Prop_Baseplate_50x50
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (50x50)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR.jpg";
+	};
+	class FST_Prop_Baseplate_50_Square: 3AS_Prop_Baseplate_50x50
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate (50x50) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		editorPreview="3as\3AS_Platforms\Platforms\EditorPreviews\3AS_Prop_Baseplate_10_GAR_Square.jpg";
+	};
+	class 3AS_Prop_Baseplate_50x50_thin;
+	class FST_Prop_Baseplate_50_Thin: 3AS_Prop_Baseplate_50x50_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (50x50)";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar\bp_50_gar.rvmat"
+		};
+	};
+	class FST_Prop_Baseplate_50_Thin_Square: 3AS_Prop_Baseplate_50x50_thin
+	{
+		author="Gold";
+		displayName="[41st] Gweem Baseplate Thin (50x50) Square";
+		editorCategory="FST_Faction";
+		editorSubcategory="FST_Baseplates";
+		hiddenSelectionsTextures[]=
+		{
+			"41st_Props\Data\Baseplates\FST_GAR_S_50BP.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"3AS\3AS_Platforms\Platforms\data\bp_50_gar_s\bp_50_gar_s.rvmat"
 		};
 	};
 };
