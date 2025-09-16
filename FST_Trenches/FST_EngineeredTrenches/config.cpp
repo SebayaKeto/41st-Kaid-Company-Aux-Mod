4d0876be057e669d064a1ee339e098bddffbe612
Maldova <62029006+WilliamHolbert@users.noreply.github.com>
2025-09-16 10:45:47 -0700

    Add engineered trench assets and config
    Introduces new textures and configuration for engineered trenches, including multiple camouflage variants and new trench vehicle classes. Updates the published modpack to include these assets.

---
+++ FST_Trenches/FST_EngineeredTrenches/config.cpp
@@ -0,0 +1,98 @@
+class CfgPatches
+{
+	class FST_Trenches_Engineered
+	{
+		author="Maldova";
+		requiredAddons[]=
+		{
+			"A3_Structures_F_Exp_Civilian",
+			"FST_Core"
+		};
+		requiredVersion=0.1;
+		units[]=
+		{
+			"FST_Trench_Engineered_Short",
+			"FST_Trench_Engineered_ArmoredShort"
+		};
+		weapons[]={};
+	};
+};
+class CfgVehicles
+{
+	class House;
+	class House_F: House
+	{
+		class DestructionEffects;
+	};
+	class Ruins_F;
+	class FST_Trench_EngineeredBase: House_F
+	{
+		editorCategory = "FST_CatagoryTrenches";
+		editorSubcategory = "FST_Subcategory_TrenchesEngineered";
+		scope = 1;
+		scopecurator = 1;
+		placement = "vertical";
+		mapSize = 1;
+		destrType = "DestructNo";
+		displayName = "Engineered Short Trench";
+		model = "FST\FST_Trenches\FST_EngineeredTrenches\FST_Trench_EngineeredShort.p3d";
+		faction = "prop";
+		eden = 1;
+	};
+	// Base Asset and Texture
+	class FST_Trench_Engineered_Short: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "Short Trench";
+	};
+	class FST_Trench_Engineered_ArmoredShort: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "Long Trench";
+		model = "FST\FST_Trenches\FST_DugTrenches\FST_Trench_Long.p3d";
+	};
+	class FST_Trench_High: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "High Trench";
+		model = "FST\FST_Trenches\FST_DugTrenches\FST_Trench_High.p3d";
+	};
+	class FST_Trench_Vehicle: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "Vehicle Trench";
+		model = "FST\FST_Trenches\FST_DugTrenches\FST_Trench_Vehicle.p3d";
+	};
+	class FST_Trench_Varactyl: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "Varactyl Trench";
+		model = "FST\FST_Trenches\FST_DugTrenches\FST_Trench_Dorito.p3d";
+	};
+	class FST_Trench_Foxhole: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "Foxhole Trench";
+		model = "FST\FST_Trenches\FST_DugTrenches\FST_Trench_Foxhole.p3d";
+	};
+	class FST_Trench_Big: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "Big Trench";
+		model = "FST\FST_Trenches\FST_DugTrenches\FST_Trench_Big.p3d";
+	};
+	class FST_Trench_Giant: FST_Trench_Base
+	{
+		scope = 2;
+		scopecurator = 2;
+		displayName = "Giant Trench";
+		model = "FST\FST_Trenches\FST_DugTrenches\FST_Trench_Giant.p3d";
+	};
+};
