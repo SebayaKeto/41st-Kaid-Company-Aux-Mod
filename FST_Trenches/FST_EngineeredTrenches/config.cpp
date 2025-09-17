a14bf41d37a56a4dcffb9fdac9615555ec4aa685
Maldova <62029006+WilliamHolbert@users.noreply.github.com>
2025-09-16 18:14:45 -0700

    Updated Config.cpp for Engineered Trenches to cut down on Texture amount

--- FST_Trenches/FST_EngineeredTrenches/config.cpp
+++ FST_Trenches/FST_EngineeredTrenches/config.cpp
@@ -46,8 +46,8 @@ class CfgVehicles
 		scopecurator = 2;
 		displayName = "Engineered Short Trench";
 		HiddenSelections[] = {"Camo","Camo1","Camo2","Camo3"};
-		HiddenSelectionsTextures[] = {"FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo_co.paa","FST\FST_Common\Data\Textures\SteelPlates\SteelPlates_co.paa""FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo2_co.paa","FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo3_co.paa"};
-		HiddenSelectionsMaterials[] = {"FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo.rvmat","FST\FST_Common\Data\Textures\SteelPlates\SteelPlates.rvmat""FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo2.rvmat","FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo3.rvmat"};
+		HiddenSelectionsTextures[] = {"FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo_co.paa","FST\FST_Common\Data\Textures\SteelPlates\SteelPlates_co.paa","FST\FST_Common\Data\Textures\SteelPlates\SteelPlates_co.paa","FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo3_co.paa"};
+		HiddenSelectionsMaterials[] = {"FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo.rvmat","FST\FST_Common\Data\Textures\SteelPlates\SteelPlates.rvmat","FST\FST_Common\Data\Textures\SteelPlates\SteelPlates.rvmat","FST\FST_Trenches\FST_EngineeredTrenches\Data\Textures\Camo3.rvmat"};
 	};
 	class FST_Trench_Engineered_ArmoredShort: FST_Trench_EngineeredBase
 	{
