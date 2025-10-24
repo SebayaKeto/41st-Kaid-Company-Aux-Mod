ac0a2c1b8896f5365b00a43b32e849084f33e7c3
Maldova <62029006+WilliamHolbert@users.noreply.github.com>
2025-10-24 09:49:35 -0700

    Add MEC Core config, functions, and briefs (Establishing MEC as a company in the Universe)
    Added initial config.cpp and functions.cpp for FST_MEC_Core, defining patch requirements and editor categories. Included two PDF briefs: MEC Corporate Brief and Naldova Planetary Profile.

---
+++ FST_MEC/FST_MEC_Core/config.cpp
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.45
+//https://mikero.bytex.digital/Downloads
+//'now' is Wed Apr 24 11:48:44 2024 : 'file' last modified on Wed Apr 17 19:43:44 2024
+////////////////////////////////////////////////////////////////////
+#include "functions.cpp"
+#define _ARMA_
+
+class CfgPatches
+{
+	class FST_MEC_Core
+	{
+		author = "Maldova";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {"A3_Data_F_Enoch_Loadorder","CBA_MAIN","FST_Core","FST_Common"};
+		requiredversion = 0.1;
+	};
+};
+class CfgEditorCategories
+{
+	class FST_MEC_CategoryStructures
+	{
+		displayname = "[MEC] Structures";
+	};
+};
+class CfgEditorSubcategories
+{
+	class FST_MEC_SubcategoryBarriers
+	{
+		displayName = "[MEC] Barriers";
+	};
+};
---
+++ FST_MEC/FST_MEC_Core/functions.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.45
+//https://mikero.bytex.digital/Downloads
+//'now' is Wed Apr 24 11:48:44 2024 : 'file' last modified on Wed Apr 17 19:43:44 2024
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class FST_MEC_Functions
+	{
+		author = "Maldova";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {"A3_Data_F_Enoch_Loadorder","CBA_MAIN","FST_Core","FST_Common"};
+		requiredversion = 0.1;
+	};
+};
