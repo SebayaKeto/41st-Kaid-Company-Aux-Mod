4de5a3d856c5fb81fdd561ef6d7a4bd5125e9dea
Maldova <62029006+WilliamHolbert@users.noreply.github.com>
2026-02-01 14:38:18 -0800

    Add FST_DC15C_VersionTwo blaster rifle config
    Introduces the FST_DC15C_VersionTwo weapon to the CfgWeapons class, including its properties, firing modes, sounds, and compatible optics. Also updates the weapons array in CfgPatches to include the new rifle.

--- FST_Weapons/FST_Primary/Config.cpp
+++ FST_Weapons/FST_Primary/Config.cpp
@@ -13,7 +13,8 @@ class CfgPatches
 		units[]={};
 		weapons[]=
 		{
-			"FST_DC15A_VersionTwo"
+			"FST_DC15A_VersionTwo",
+			"FST_DC15C_VersionTwo"
 		};
 		magazines[]=
 		{
@@ -111,6 +112,69 @@ class CfgWeapons
 			{
 				linkProxy = "\A3\data_f\proxies\weapon_slots\SIDE";
 				compatibleItems[] = {};
+			};
+		};
+	};
+	class FST_DC15C_VersionTwo: Rifle_Base_F
+	{
+		author = "Maldova";
+		scope = 2;
+		displayName = "[41st] DC-15C Blaster Rifle";
+		descriptionShort = "DC-15C Blaster Rifle";
+		model = "\FST\FST_Weapons\FST_Primary\DC15C.p3d";
+		picture = "FST\FST_Weapons\FST_Primary\Icons\DC15CIcon.paa";
+		baseWeapon = "FST_DC15C_VersionTwo";
+		magazineWell[] = {"FST_DC15AMagwell"};
+		modes[] = {"Single","FullAuto"};
+		class FullAuto: Mode_FullAuto
+		{
+			reloadTime = 0.2;
+			dispersion = 0.00066;
+			minRange = 0;
+			minRangeProbab = 0.9;
+			midRange = 15;
+			midRangeProbab = 0.7;
+			maxRange = 30;
+			maxRangeProbab = 0.1;
+			soundContinuous = 0;
+			soundBurst = 0;
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType;
+			class StandardSound: BaseSoundModeType
+			{
+				soundSetShot[] = {"FST_DC15Soundset_Shot"};
+			};
+		};
+		class Single: Mode_SemiAuto
+		{
+			reloadTime = 0.05;
+			dispersion = 0.00066;
+			minRange = 2;
+			minRangeProbab = 0.5;
+			midRange = 200;
+			midRangeProbab = 0.7;
+			maxRange = 400;
+			maxRangeProbab = 0.3;
+			soundContinuous = 0;
+			soundBurst = 0;
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType;
+			class StandardSound: BaseSoundModeType
+			{
+				soundSetShot[] = {"FST_DC15Soundset_Shot"};
+			};
+		};
+		class WeaponSlotsInfo: WeaponSlotsInfo
+		{
+			mass = 80;
+			class CowsSlot: CowsSlot
+			{
+				linkProxy = "\A3\data_f\proxies\weapon_slots\TOP";
+				compatibleItems[] = 
+				{
+					"3AS_Optic_Red_DC15A",
+					"3AS_Optic_LEScope_DC15A"
+				};
 			};
 		};
 	};
