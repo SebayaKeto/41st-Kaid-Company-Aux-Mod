85aa982056df9ae9829a0ca562d26d923504c6f3
Maldova <62029006+WilliamHolbert@users.noreply.github.com>
2026-02-01 19:19:05 -0800

    Add DC-15S Blaster Rifle and assets
    Introduces the DC-15S Blaster Rifle to the mod, including its model, textures, and configuration in Config.cpp. Updates model.cfg to support the new weapon model.

--- FST_Weapons/FST_Primary/Config.cpp
+++ FST_Weapons/FST_Primary/Config.cpp
@@ -178,4 +178,67 @@ class CfgWeapons
 			};
 		};
 	};
+	class FST_DC15S_VersionTwo: Rifle_Base_F
+	{
+		author = "Maldova";
+		scope = 2;
+		displayName = "[41st] DC-15S Blaster Rifle";
+		descriptionShort = "DC-15S Blaster Rifle";
+		model = "\FST\FST_Weapons\FST_Primary\DC15S.p3d";
+		picture = "FST\FST_Weapons\FST_Primary\Icons\DC15SIcon.paa";
+		baseWeapon = "FST_DC15S_VersionTwo";
+		magazineWell[] = {"FST_DC15AMagwell"};
+		modes[] = {"Single","FullAuto"};
+		class FullAuto: Mode_FullAuto
+		{
+			reloadTime = 0.2;
+			dispersion = 0.00066;
+			minRange = 0;
+			minRangeProbab = 0.9;
+			midRange = 15;
+			midRangeProbab = 0.7;
+			maxRange = 30;
+			maxRangeProbab = 0.1;
+			soundContinuous = 0;
+			soundBurst = 0;
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType;
+			class StandardSound: BaseSoundModeType
+			{
+				soundSetShot[] = {"FST_DC15Soundset_Shot"};
+			};
+		};
+		class Single: Mode_SemiAuto
+		{
+			reloadTime = 0.05;
+			dispersion = 0.00066;
+			minRange = 2;
+			minRangeProbab = 0.5;
+			midRange = 200;
+			midRangeProbab = 0.7;
+			maxRange = 400;
+			maxRangeProbab = 0.3;
+			soundContinuous = 0;
+			soundBurst = 0;
+			sounds[] = {"StandardSound"};
+			class BaseSoundModeType;
+			class StandardSound: BaseSoundModeType
+			{
+				soundSetShot[] = {"FST_DC15Soundset_Shot"};
+			};
+		};
+		class WeaponSlotsInfo: WeaponSlotsInfo
+		{
+			mass = 80;
+			class CowsSlot: CowsSlot
+			{
+				linkProxy = "\A3\data_f\proxies\weapon_slots\TOP";
+				compatibleItems[] = 
+				{
+					"3AS_Optic_Red_DC15A",
+					"3AS_Optic_LEScope_DC15A"
+				};
+			};
+		};
+	};
 };
